7502a6c07ef803fcec4f7d1db1307b77072ad88d
sitakura <scot.itakura@gmail.com>
2020-11-15 22:45:24 -0800

    added index connections and databass

--- index.js
+++ index.js
@@ -29,3 +29,51 @@ con.promise()
   .then(() => main())
 
   .catch(console.error)
+  function main() {
+      return con.promise().query(
+          'SELECT \
+              e.id, e.first_name, e.last_name, r.title, \
+              d.name AS department, r.salary, \
+              CONCAT(m.first_name, " ", m.last_name) AS manager \
+          FROM employee e \
+          LEFT JOIN employee m ON m.id = e.manager_id \
+          LEFT JOIN role r ON r.id = e.role_id \
+          LEFT JOIN department d ON d.id = r.department_id;'
+      )
+      .then(([rows, fields]) => console.table('\nAll Employees', rows))
+      .then(() => prompt())
+  }
+  
+  function prompt() {
+      inquirer.prompt([{
+          type: 'list',
+          name: 'obj',
+          message: 'What would you like to do?',
+          choices: [
+              {
+                  name: 'Add Department',
+                  value: Department
+              },
+              {
+                  name: 'Add Role',
+                  value: Role
+              },
+              {
+                  name: 'Add Employee',
+                  value: Employee
+              },
+              {
+                  name: "I'm done",
+                  value: null
+              }
+          ]
+      }]).then(response => {
+          if (response.obj != null) {
+              response.obj.add(con)
+                  .then(() => main())
+          } else {
+              con.end()
+          }
+      })
+  }
+  
