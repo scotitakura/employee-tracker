77ee35d8fbea96004ed3062ac80d84d85cfb0a5f
sitakura <scot.itakura@gmail.com>
2020-11-15 21:09:43 -0800

    const requirements

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer')
+const mysql = require('mysql2')
+
+const Department = require('./lib/Department.js')
+const Role = require('./lib/Role.js')
+const Employee = require('./lib/Employee.js')
